47dc76f0246c5fa209739568d852150eb03259f6
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2023-11-09 15:31:09 -0800

    Finished questions

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -7,15 +7,15 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let firstItem = arr[0]
 
 ////////// PROBLEM 2 //////////
 /*
   Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
 */
-
 //Code Here
-
+let lastItemRemoved = arr.splice(arr.length - 1, 1)
+console.log(arr)
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -27,7 +27,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for (i = 0; i < family.length; i++) {
+  console.log(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -43,7 +45,11 @@ let evensArr = []
 
 //Code Here
 
-
+for (i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 0) {
+    evensArr.push(nums[i])
+  }
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -57,6 +63,17 @@ var score = 74
 
 //Code Here
 
+if (score >= 90) {
+  console.log('A')
+} else if (score >= 80) {
+  console.log('B')
+} else if (score >= 70) {
+  console.log('C')
+} else if (score >= 60) {
+  console.log('D')
+} else {
+  console.log('F')
+}
 
 ////////// Intermediate Problems //////////
 
@@ -72,15 +89,21 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+let someNum = myFavoriteNumbers[4]
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
+// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that.
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
 
+if (myFavoriteNumbers.length < 7) {
+  console.log('There are not enough elements in this array')
 
+} else {
+  someNum = myFavoriteNumbers[6]
+}
 ////////// PROBLEM 8 //////////
 
 // Do not edit the code below.
@@ -91,6 +114,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+for (i = 0; i < listOfNumbers.length; i++) {
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(` ${listOfNumbers[i]} is divisible by 3.`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -102,6 +130,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+for (i = letters.length - 1; i >= 0; i--) {
+  console.log(letters[i])
+}
 
 
 ////////// Advanced Problems //////////
@@ -125,7 +156,22 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
-
+switch(letterGrade) {
+  case 'A':
+    console.log("The student is doing excellently.")
+    break;
+  case 'B':
+    console.log("The student is doing well.")
+    break;
+  case 'C':
+    console.log("The student is doing alright.")
+    break;
+    case 'D':
+    console.log("The student is not doing very well.")
+    break;
+  default:
+    console.log("The student is failing.")
+}
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
@@ -152,3 +198,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for(i = 1; i < 100; i++) {
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log("DevMountain")
+  } else if(i % 3 === 0) {
+    console.log("Dev")
+  } else if(i % 5 === 0) {
+    console.log("Mountain")
+  } else {
+    console.log(i)
+  }
+}
